fb5758aec1be0264baf8369082af2c818ae82698
Samuel Vilkovský <samuel.vilkovsky.2@student.tuke.sk>
2023-08-10 18:19:54 +0200

    changed capital

--- med-app-backend/routes/questions.js
+++ med-app-backend/routes/questions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('../models/question');
+const Question = require('../models/Question');
 
 // Create a new question
 router.post('/', async (req, res) => {
